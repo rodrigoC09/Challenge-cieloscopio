c8ee500744863d2638856fd8b0b225e29dac28db
rodrigoC09 <rodrigo.antonio.caro@gmail.com>
2026-04-24 12:07:16 -0400

    proyecto base cieloscopio

---
+++ src/main/java/com/cieloscopio/cieloscopio/CieloscopioApplication.java
@@ -0,0 +1,13 @@
+package com.cieloscopio.cieloscopio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CieloscopioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CieloscopioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cieloscopio/cieloscopio/CieloscopioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cieloscopio.cieloscopio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CieloscopioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
